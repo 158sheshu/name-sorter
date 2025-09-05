9e4377d0f8b6934c4ed0b7c1f9bfba54f17a3ae6
amire@VVSAmireddy <rsathvik2017@gmail.com>
2025-09-05 01:49:57 -0400

    Comments added for the Code.

--- src/com/ddns/namesorter/service/NameSorterService.java
+++ src/com/ddns/namesorter/service/NameSorterService.java
@@ -4,6 +4,17 @@ import com.ddns.namesorter.model.PersonName;
 
 import java.util.List;
 
+/**
+ * Service interface for sorting names.
+ */
 public interface NameSorterService {
+
+    /**
+     * Sorts a list of PersonName objects first by last name, then by given names.
+     *
+     * @param names List of PersonName objects to be sorted.
+     * @return A new list of PersonName objects sorted by last name and given names.
+     */
     List<PersonName> sortNames(List<PersonName> names);
+
 }
